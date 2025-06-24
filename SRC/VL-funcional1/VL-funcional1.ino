978e529eeb7f591096cbbb67478a3b98934c71cf
joseleomarquina <joseleonardomarq10@gmail.com>
2025-06-24 19:25:24 -0400

    SUBIENDO CODIGO

--- SRC/VL-funcional1/VL-funcional1.ino
+++ SRC/VL-funcional1/VL-funcional1.ino
@@ -1,6 +1,6 @@
 //CARRO-ARDUINO
 
-  #include <Servo.h>
+#include <Servo.h>
   #include <NewPing.h>
   Servo servo1Motor;
   Servo servo2Motor;
@@ -24,21 +24,13 @@
  // Servo direccion;
 
   // Variables globales
-  int CRUCES_ESQUINA = 0;
-  int CRUCES_NIVELACION = 0;
-  int DISTANCIAINICIAL;
+  int cruces = 0;
   const int DISTANCIA_MINIMA = 50; // Distancia mínima antes de girar (en cm)
-  const int TIEMPO_ESPERA_INICIAL = 1000; // 3 segundos de espera inicial
+  const int TIEMPO_ESPERA_INICIAL = 3000; // 3 segundos de espera inicial
   const int CRUCES_MAXIMOS = 12;
-  int Arranque=0;
 
   void setup() {
-    pinMode(4,INPUT_PULLUP);
-    while(Arranque==0){
-      if(digitalRead(4)==LOW){
-        Arranque=1;
-      }
-    }
+  
     
     // Configurar pines del motor
     pinMode(MOTOR_IN1, OUTPUT);
@@ -51,61 +43,44 @@
     // Esperar 3 segundos antes de arrancar
     delay(TIEMPO_ESPERA_INICIAL);
     Serial.begin(9600);
-    Serial.println("Sensor Ultrasonico Hc-SR04");
-    Serial.println("Write by Guille and Leo");
-    DISTANCIAINICIAL = sonar.ping_cm();
+    Serial.println("Ultrasonic Sensor Hc-SR04 Test");
+    Serial.println("Write by Guille and Luis");
+    moverAdelante();
+    delay(500);
+    moverAtras();
+    delay(500);
+    detenerCarro();
+    delay(1000);
+    girarIzquierda();
+    delay(1000);
+    girarDerecha();
+    delay(1000);
+    centrarDireccion();
+    delay(1000);
   }
 
   void loop() {
-    // Medir la distancia
-    int DISTANCIA = sonar.ping_cm();
-    if (CRUCES_ESQUINA >= CRUCES_MAXIMOS) {
-      if(DISTANCIA<=DISTANCIAINICIAL){
-       detenerCarro();
-       return; // Terminar la ejecución
-      }
+    if (cruces >= CRUCES_MAXIMOS) {
+      detenerCarro();
+      return; // Terminar la ejecución
     }else{
       // Mover el carro hacia adelante
       moverAdelante();
     }
-    
-    // Medir la distancia
-    DISTANCIA = sonar.ping_cm();
-    // Si detecta un obstáculo, girar a la izquierda
-    if ((DISTANCIA > 0) && (DISTANCIA <= DISTANCIA_MINIMA)) {
-      girarIzquierda();
-      CRUCES_ESQUINA++;
-      delay(1000); // Esperar un segundo después de girar
-      centrarDireccion();
-      delay(500);
-      apuntarFullDerecha();
-      
-      if (DISTANCIA > 15){
-        nivelarParedD();
-        CRUCES_NIVELACION++;
-        delay(250);
-      }
-        else {
-          centrarDireccion();
-      }
 
-      if (DISTANCIA <10){
-        nivelarParedI();
-        CRUCES_NIVELACION++;
-        delay(250);
-      }
-        else {
-          centrarDireccion();
-      }
-      delay(500);
-      apuntarFullCentro();
+    // Medir la distancia
+    int distancia = sonar.ping_cm();
+    // Si detecta un obstáculo, girar a la izquierda
+    if ((distancia > 0) && (distancia <= DISTANCIA_MINIMA)) {
+      girarIzquierda();
+      cruces++;
+      delay(1000); // Esperar un segundo después de girar
       centrarDireccion();
       delay(100);
       Serial.print("Distancia: ");
-      Serial.println(DISTANCIA);
+      Serial.println(distancia);
       Serial.print("Cruces: ");
-      Serial.println(CRUCES_ESQUINA);
-    
+      Serial.println(cruces);
     }
   }
 
@@ -138,27 +113,5 @@
 
   void centrarDireccion() {
     Serial.println("Centrar");
-    servo1Motor.write(90);
-  }
-
-  void apuntarFullDerecha() {
-    Serial.println("FullDerecha");
-    servo2Motor.write(1);
-  }
-
-  void apuntarFullIzquierda() {
-    Serial.println("FullIzquierda");
-    servo2Motor.write(135);
-  }
-  void apuntarFullCentro() {
-    Serial.println("FullCentro");
-    servo2Motor.write(90);
-  }
-  void nivelarParedD() {
-    Serial.println("NivelandoD");
-    servo1Motor.write(95);
-  }
-  void nivelarParedI() { 
-    Serial.println("NivelandoI");
-    servo1Motor.write(80);
+    servo1Motor.write(88);
   }
